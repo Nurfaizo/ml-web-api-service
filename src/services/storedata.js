194390358e09c77b0daa5d0e17a769a11400b6a8
M Nurfaiz <112399298+Nurfaizo@users.noreply.github.com>
2024-12-08 07:46:36 +0700

    Update storedata.js

--- src/services/storedata.js
+++ src/services/storedata.js
@@ -1,13 +1,34 @@
-// src/services/storeData.js
-let predictions = [];
+const { Firestore } = require('@google-cloud/firestore');
+
+
+const db = new Firestore();
 
 async function storeData(id, data) {
-    predictions.push({ id, ...data });
-    return { id, ...data };
+    try {
+        const predictCollection = db.collection('predictions');
+        await predictCollection.doc(id).set(data); 
+        return { id, ...data };
+    } catch (error) {
+        console.error('Error storing data in Firestore:', error);
+        throw new Error('Failed to store data');
+    }
 }
 
+
 async function getAllData() {
-    return predictions;
+    try {
+        const predictCollection = db.collection('predictions');
+        const snapshot = await predictCollection.get(); 
+        if (snapshot.empty) {
+            console.log('No predictions found.');
+            return []; 
+        }
+        return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })); 
+    } catch (error) {
+        console.error('Error fetching data from Firestore:', error);
+        throw new Error('Failed to fetch data');
+    }
 }
+
 
 module.exports = { storeData, getAllData };

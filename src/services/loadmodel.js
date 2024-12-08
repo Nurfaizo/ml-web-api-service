b49a05a541d2fb15e637eb591bf789820c22ebee
M Nurfaiz <112399298+Nurfaizo@users.noreply.github.com>
2024-12-08 08:58:56 +0700

    Update loadmodel.js

--- src/services/loadmodel.js
+++ src/services/loadmodel.js
@@ -1,15 +1,32 @@
-// src/services/loadmodel.js
 const tf = require('@tensorflow/tfjs-node');
+const { Storage } = require('@google-cloud/storage');
 const path = require('path');
 
 async function loadModel() {
     try {
-        // Load the model from a local path
-        const modelPath = path.join(__dirname, '../../models/model.json');
-        return await tf.loadGraphModel(`file://${modelPath}`);
+
+        const storage = new Storage({
+            keyFilename: path.join(__dirname, '../../submissionmlgc-nurfaiz-cfd2d511e676.json'), 
+        });
+
+        const bucketName = 'ml-web-storage-model'; 
+        const modelJsonFile = 'models/model.json'; 
+        const [modelJsonUrl] = await storage
+            .bucket(bucketName)
+            .file(modelJsonFile)
+            .getSignedUrl({
+                action: 'read',
+                expires: Date.now() + 15 * 60 * 1000, 
+            });
+
+        console.log(`Generated signed URL for model.json: ${modelJsonUrl}`);
+
+        const model = await tf.loadGraphModel(modelJsonUrl);
+        console.log('Model successfully loaded from GCS');
+        return model;
     } catch (error) {
-        console.error('Error loading the model:', error);
-        throw new Error('Failed to load the model');
+        console.error('Error loading the model from GCS:', error);
+        throw new Error('Failed to load the model from GCS');
     }
 }
 
